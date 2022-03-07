e56db418ad9a971a3141ddf7d10b2df8bfe414ba
Drew Mendenhall <drew@mendenhall.io>
2022-03-06 23:23:58 -0700

    Add Import Ordering Rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,21 @@ const config = {
     'eslint:recommended',
     'next/core-web-vitals',
   ],
+
+  rules: {
+    'import/order': ['warn', {
+      'alphabetize': { order: 'asc' },
+      'groups': [
+        'builtin',
+        'external',
+        'internal',
+        'parent',
+        'sibling',
+        'type',
+      ],
+      'newlines-between': 'always',
+    }],
+  },
 }
 
 module.exports = config
